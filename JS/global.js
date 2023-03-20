07d2a094db88cfd21886be92cf15ecee5588c67c
0xAntoDev <antoinelegy@gmail.com>
2023-03-20 12:51:06 +0100

    patch click buttom / new hamburger menu animation and update page

--- JS/global.js
+++ JS/global.js
@@ -1,11 +1,18 @@
-let burgerMenu = document.getElementById("burgerMenu")
 let mobileBurgerMenu = document.getElementById("mobileBurgerMenu")
 let iconBurger = document.getElementById("iconBurger")
 let iconCross = document.getElementById("iconCross")
 
-burgerMenu.addEventListener("click", function () {
-    mobileBurgerMenu.classList.toggle("display_none")
-    document.body.classList.toggle("no_overflow")
-    iconBurger.classList.toggle("display_none")
-    iconCross.classList.toggle("display_none")
-})
+// burgerMenu.addEventListener("click", function () {
+   
+//     iconBurger.classList.toggle("display_none")
+//     iconCross.classList.toggle("display_none")
+// })
+
+
+document.querySelectorAll('.menu').forEach(btn => {
+    btn.addEventListener('click', e => {
+        btn.classList.toggle('active');
+        mobileBurgerMenu.classList.toggle("display_none")
+        document.body.classList.toggle("no_overflow")
+    });
+});
